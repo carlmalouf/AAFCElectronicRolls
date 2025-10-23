3934de81bbf0bd30923599c5c970c9e362ef9bb8
Carl Malouf <carlmalouf@hotmail.com>
2025-10-24 09:39:44 +1000

    Remove .bat and .ps1 files from repository

--- run_app.ps1
+++
@@ -1,6 +0,0 @@
-# Activate virtual environment and run Streamlit app
-.\venv\Scripts\Activate.ps1
-Write-Host "Virtual environment activated!" -ForegroundColor Green
-Write-Host ""
-Write-Host "Starting Streamlit application..." -ForegroundColor Cyan
-streamlit run app.py
